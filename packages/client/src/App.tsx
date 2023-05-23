74a1a527a9483da67604aa349a3ca4491710cb8f
shyam-patel-kira <shyampkira@gmail.com>
2023-05-23 11:17:35 -0400

    Add cities and citizen counters

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,5 @@
-//import { useComponentValue } from "@latticexyz/react";
-//import { useMUD } from "./MUDContext";
+import { useComponentValue, useRows } from "@latticexyz/react";
+import { useMUD } from "./MUDContext";
 import { useNavigate } from 'react-router-dom';
 
 export const App = () => {
@@ -10,17 +10,22 @@ export const App = () => {
   const goToNewPlayer = () => {
     navigate('/NewPlayer');
   };
-// const {
-//    components: { Counter },
-//    systemCalls: { increment },
-//    network: { singletonEntity },
-//  } = useMUD();
-//
-//  const counter = useComponentValue(Counter, singletonEntity);
+  const {
+    components: { Citizens, CitizensCounter, Proposals, ProposalCounter, CitiesCounter, Voting, Cities, VerifiedCities, SupercitiesTokenTable },
+    network: { singletonEntity, worldSend, storeCache },
+  } = useMUD();
+
+  const citizensCounter = useComponentValue(CitizensCounter, singletonEntity);
+  const citiesCounter = useComponentValue(CitiesCounter, singletonEntity);
+  const proposals = useComponentValue(Proposals, singletonEntity);
+  // const superCitiesTokenTable = useRows(storeCache, { table : "SuperCitiesToken" });
+
+  console.log(CitizensCounter);
 
   return (
     <div className="h-screen bg-bgd text-white pt-20 px-3 grid text-center gap-3 items-center">
         <div className='font-nab text-9xl pt-24' >Super Cities</div>
+        <div> CitizenID: {Citizens?.id} </div>
         <div className='flex mx-auto gap-5'>
           <button className='border-2 mx-auto rounded-2xl text-2xl font-nova py-2 px-6 hover:bg-zen hover:text-bgd' onClick={goToNewPlayer}>New Player</button>
           <button className='border-2 mx-auto rounded-2xl text-2xl font-nova py-2 px-6 hover:bg-zen hover:text-bgd' onClick={goToAllCities}>Load Game</button>
